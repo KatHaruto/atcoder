933dd48e48c3e264f1bf71588cc8ffc549a00036
tr-author <katsushiro@g.ecc.u-tokyo.ac.jp>
2022-01-13 22:56:02 +0900

    d

--- abc152/d/main.py
+++ abc152/d/main.py
@@ -1,21 +1,12 @@
-N = int(input())
-cnt = 0
-for a in range(1, N + 1):
-    if a < 10:
-      cnt += 1
-    if str(a)[-1] == "0":
-        continue
-    if len(str(a)) < len(str(N)):
-        if len(str(a)) == 2:
-          cnt += 3
-        elif len(str(a)) == 3:
-          cnt += 
-        if str(a)[0] == str(a)[-1]:
-            cnt += 2*(1+int('1'*len(str(a))-2)+int(str(a)]))
-        cnt += 10 ** (max(0, len(str(a)) - 2)) + int(str(a)[0] != str(a)[-1])
-    else:
-        if str(N)[0] < str(a)[-1] or str(N)[-1] < str(a)[0]:
-            continue
-        # cnt +=
+from itertools import product
 
+N = int(input())
+num = [[0] * 10 for _ in range(10)]
+
+for i in range(1, N + 1):
+    if i % 10 != 0:
+        num[int(str(i)[0])][i % 10] += 1
+cnt = 0
+for i, j in product(range(1, 10), repeat=2):
+    cnt += num[i][j] * num[j][i]
 print(cnt)
